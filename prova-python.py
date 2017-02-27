8ad66f428d5f89c81313bd1697411a5bf5af0a2d
tykos82 <tykosvarie@gmail.com>
2017-02-27 15:56:06 +0100

    Update prova-python.py

--- prova-python.py
+++ prova-python.py
@@ -1,9 +1,13 @@
 import sys
 sys.path.insert(0, 'lib')
+import urllib
 
 from bs4 import BeautifulSoup
 url = 'https://www.frenf.it/earlyadopters/rss/AsiaOverland2002'
-soup = BeautifulSoup(url, 'xml')
+res = urllib.urlopen(url)
+html = res.read()
+
+soup = BeautifulSoup(html, 'lxml')
 
 giornata = soup.findAll('item')
 
