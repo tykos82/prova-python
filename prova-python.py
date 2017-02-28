3bbedd6293dd05fdd78158f7c7f35fd88bcc8e18
tykos82 <tykosvarie@gmail.com>
2017-02-28 14:20:47 +0100

    Update prova-python.py

--- prova-python.py
+++ prova-python.py
@@ -1,18 +1,26 @@
 import sys
 sys.path.insert(0, 'lib')
 import urllib
+#import HTMLParser
 
 from bs4 import BeautifulSoup
 url = 'https://www.frenf.it/earlyadopters/rss/AsiaOverland2002'
 res = urllib.urlopen(url)
-html = res.read().decode('utf-8')
+html = res.read()
 
 soup = BeautifulSoup(html, 'xml')
 
 giornata = soup.findAll('item')
+#PROVE ENCODING un = HTMLParser.HTMLParser().unescape(soup)
+#PROVE ENCODING print un
+#PROVE ENCODING print('----------------------------------------------PAUSA---------------------------------')
+#print(soup.prettify())
 
 for ognigiornata in giornata:
-	titolo = ognigiornata('title')
-	print titolo
-	link = ognigiornata('link')
-	print link
+  if ognigiornata.creator.string == 'orizzontintorno':
+   # titolo = ognigiornata.string('title')
+    print ognigiornata.title.string
+    link = ognigiornata('link')
+    print link
+    timestamp = ognigiornata('date')
+    print timestamp
